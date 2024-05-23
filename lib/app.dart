2bdabd76f2c778ae2155d68c003fddd2e2f04c56
krysz123 <konradrysz3ct@gmail.com>
2024-05-23 14:32:31 +0200

    Create basic theme and utils

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:enginner_project/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: AppTheme.darkTheme,
+    );
+  }
+}
---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,19 @@
+class Sizes {
+  //PADDING, MARGIN SIZES
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  //ICON SIZES
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+  //FONT SIZES
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
+}
